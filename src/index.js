01a6388c5410ef367b143b10b8e5e03f7cff8b78
YuriySuro <suro1989@ukr.net>
2020-01-30 14:15:44 +0200

    simple edits to component

--- src/index.js
+++ src/index.js
@@ -4,28 +4,35 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/index.css';
 
 
-const TodoList = () => (
-    <ul className="list-group">
-        <li className="list-group-item list-group-item-action list-group-item-secondary">Learn React</li>
-        <li className="list-group-item list-group-item-action list-group-item-success">Build Awesome App</li>
-    </ul>
-);
+const TodoList = () => {
+    const items = ['Drink Coffee', 'Build Awesome App'];
+    return (
+        <ul className="list-group">
+            <li className="list-group-item list-group-item-action list-group-item-secondary">{items[0]}</li>
+            <li className="list-group-item list-group-item-action list-group-item-success">{items[1]}</li>
+        </ul>
+    )
+};
 
 const AppHeader = () => (
     <h1>My Todo List</h1>
 );
 
-const SearchPanel = () => (
-    <input type="search" placeholder="search" />
-);
+const SearchPanel = () => {
+    const searchText = "Type here to search";
+    return (
+        <input type="search" placeholder={searchText} />
+    )
+};
 
-const App = () => (
-    <div className="container">
-        <AppHeader />
-        <SearchPanel />        
-        <TodoList />
-    </div>
-);
-
+const App = () => {
+    return (
+        <div className="container">
+            <AppHeader />
+            <SearchPanel />        
+            <TodoList />
+        </div>
+    );
+};
 
 ReactDOM.render(<App />, document.querySelector("#root"));
