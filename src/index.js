c6cb85ddf979b9a2ea994c71095bafef2bd970eb
YuriySuro <suro1989@ukr.net>
2020-01-30 12:52:51 +0200

    basic components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/index.css';
 
-//const elem = React.createElement('h1', null, 'Hello World!');
-const elem = <h1>Hello World!</h1>
-console.log(elem);
 
-ReactDOM.render(elem, document.querySelector("#root"));
+const TodoList = () => (
+    <ul className="list-group">
+        <li className="list-group-item list-group-item-action list-group-item-secondary">Learn React</li>
+        <li className="list-group-item list-group-item-action list-group-item-success">Build Awesome App</li>
+    </ul>
+);
+
+const AppHeader = () => (
+    <h1>My Todo List</h1>
+);
+
+const SearchPanel = () => (
+    <input type="search" placeholder="search" />
+);
+
+const App = () => (
+    <div className="container">
+        <AppHeader />
+        <SearchPanel />        
+        <TodoList />
+    </div>
+);
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
